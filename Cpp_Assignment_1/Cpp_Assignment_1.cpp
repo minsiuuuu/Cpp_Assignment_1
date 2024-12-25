28c6e4fe3da9cc1eda07b1429b8d91798634c058
minsiuuuu <kimms010107@gmail.com>
2024-12-25 22:54:07 +0900

    도전과제 제출

--- Cpp_Assignment_1/Cpp_Assignment_1.cpp
+++ Cpp_Assignment_1/Cpp_Assignment_1.cpp
@@ -30,6 +30,33 @@ double getAvg(int sum, int size)
     return double(sum) / size;
 }
 
+// 도전과제
+vector<int> sort(vector<int> arr, int n)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr.size() - i - 1; j++)
+        {
+            bool condition = false;
+            if (n == 1) // 오름차순
+            {
+                condition = arr[j] > arr[j + 1];
+            }
+            else if (n == 2) // 내림차순
+            {
+                condition = arr[j] < arr[j + 1];
+            }
+            if (condition)
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
 int main()
 {
     // 필수과제
@@ -48,4 +75,32 @@ int main()
 
     cout << "합: " << sum << endl;
     cout << "평균: " << getAvg(sum, n) << endl;
+
+    // 도전과제
+    int s;
+    cout << "정렬 순서를 입력하세요 (오름차순: 1, 내림차순: 2): ";
+    cin >> s;
+
+    while (s != 1 && s != 2)
+    {
+        cout << "잘못된 입력입니다. 다시 입력하세요(오름차순: 1, 내림차순: 2): ";
+        cin >> s;
+    }
+
+    vector<int> sortedArr = sort(arr, s);
+
+    if (s == 1)
+    {
+        cout << "오름차순 정렬: ";
+    }
+    else if (s == 2)
+    {
+        cout << "내림차순 정렬: ";
+    }
+
+    for (int num : sortedArr)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
 }
